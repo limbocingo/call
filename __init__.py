3ee5bf4e49a748da09173bbbcff0628e8da84a28
mrcingo <mrcingosup@gmail.com>
2022-10-17 19:24:59 +0200

    sockets base script

---
+++ __init__.py
@@ -0,0 +1,1 @@
+from source.server import *
---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name = 'fassy',
+    version = '1.2',
+    description = 'Make API\'s easyer.',
+    url = 'https://github.com/limbocingo/fassy',
+    author = 'limbocingo',
+    packages = find_packages(),
+    python_requires=">=3.10",
+)
